91eb1a0b7d6e08c68c3f49824d91ea1fa1baca3b
Serban Tudor Gabriel <tudor.gabriel.serban@gmail.com>
2022-10-21 17:58:16 +0300

    0.1.1 - Added Migrations for Documents Comments and Rating - Added models for Documents comments and rating

---
+++ app/Models/DocumentsRatings.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DocumentsRatings extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'content',
+        'rating',
+        'created_by',
+        'updated_by',
+        'document_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization. 
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'id',
+
+    ];
+
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'documents_ratings';
+}
